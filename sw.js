35782204b3ce7c4641a5015b9db8e39b87eb09c0
koksharov123456-hue <koksharov123456@gmail.com>
2026-09-14 12:16:14 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'trainer-pwa-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(cached => cached || fetch(event.request))
+    );
+  }
+});
